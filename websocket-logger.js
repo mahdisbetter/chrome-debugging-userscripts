d77a2f733f8266fcbf86396a757af5ae7f0f3612
muhammad mahdi ahmed nasir abdul ibn abi <mahdi1337zz@gmail.com>
2024-09-01 17:41:47 +0200

    Update websocket-logger.js

--- websocket-logger.js
+++ websocket-logger.js
@@ -4,61 +4,78 @@
 // @run-at       document-start
 // ==/UserScript==
 
-window.loggedSockets = [];
+window.loggedWSHook = null;
+window.loggedWSMessageHook = null;
+
+window.loggedWebSockets = [];
 
 const OriginalWebSocket = WebSocket;
 
 function arrayBufferToHexString(buffer) {
     const byteArray = new Uint8Array(buffer);
     let hexString = '';
+
     for (let i = 0; i < byteArray.length; i++) {
         const hex = byteArray[i].toString(16).padStart(2, '0');
         hexString += hex;
     }
-    return hexString
+
+    return hexString;
 }
 
 function arrayBufferToB64String(buffer) {
     const byteArray = new Uint8Array(buffer);
     let binaryString = '';
+
     for (let i = 0; i < byteArray.length; i++) {
         binaryString += String.fromCharCode(byteArray[i]);
     }
+
     return btoa(binaryString);
 }
 
-window.WebSocket = function(url, protocols) {
+window.WebSocket = function (url, protocols) {
     const ws = new OriginalWebSocket(url, protocols);
 
-    const creationStackTrace = (new Error());
+    const creationStackTrace = new Error();
     const timestamp = new Date().toISOString();
 
     const loggedSocket = {
         socket: ws,
         stack: creationStackTrace,
         timestamp: timestamp,
-        loggedMessages: []
+        loggedMessages: [],
     };
 
-    window.loggedSockets.push(loggedSocket);
+    window.loggedWebSockets.push(loggedSocket);
+
+    if (typeof window.loggedWSHook === 'function') {
+        window.loggedWSHook(loggedSocket);
+    }
 
     const originalSend = ws.send;
-    ws.send = function(data) {
-        const sendStackTrace = (new Error());
+
+    ws.send = function (data) {
+        const sendStackTrace = new Error();
         const timestamp = new Date().toISOString();
 
-        loggedMessage = {
+        let loggedMessage = {
             message: data,
             stack: sendStackTrace,
-            timestamp: timestamp
-        }
+            timestamp: timestamp,
+            socket: loggedSocket,
+        };
 
         if (loggedMessage.message instanceof ArrayBuffer) {
-          loggedMessage.message.hexString = arrayBufferToHexString(loggedMessage.message)
-          loggedMessage.message.b64String = arrayBufferToB64String(loggedMessage.message)
+            loggedMessage.message.hexString = arrayBufferToHexString(loggedMessage.message);
+            loggedMessage.message.b64String = arrayBufferToB64String(loggedMessage.message);
         }
 
         loggedSocket.loggedMessages.push(loggedMessage);
+
+        if (typeof window.loggedWSMessageHook === 'function') {
+            window.loggedWSMessageHook(loggedMessage);
+        }
 
         return originalSend.apply(ws, arguments);
     };
